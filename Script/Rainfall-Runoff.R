262d7f56a2b37ff158de6cc261a36034bc897899
Holly Bacon <hollybacon@Hollys-MacBook-Air-40.local>
2024-11-20 23:27:13 +0000

    Making model

--- Script/Rainfall-Runoff.R
+++ Script/Rainfall-Runoff.R
@@ -3,12 +3,12 @@
 
 # ---- Library ----
 
-install.packages("nasapower")
 library(nasapower)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(tidyverse)
+library(lubridate)
 
 # ---- Load data ----
 
@@ -42,26 +42,26 @@ Precipitation_clean <- Precipitation[20:nrow(Precipitation), ]
 
 # Name them
 
-colnames(Discharge_clean) <- c("Date", "Discharge_m3/s")
+colnames(Discharge_clean) <- c("Date", "Discharge_m3pers")
 
 colnames(Precipitation_clean) <- c("Date", "Precipitation_mm")
 
 # Select necessary columns
 
 Discharge_clean <- Discharge_clean %>%
-  select(Date, Discharge_m3/s )
+  select(Date, Discharge_m3pers )
 
 Precipitation_clean <- Precipitation_clean %>%
-  select(Date, `Precipitation_mm )
+  select(Date, Precipitation_mm )
 
 Evapotranspiration_clean <- Evapotranspiration %>%
   select(YYYYMMDD, EVPTRNS)
 
 colnames(Evapotranspiration_clean) <- c("Date", "Evapotranspiration_mm")
 
-# ---- Changing each variable into the same unit ----
+# ---- Fixing units ----
 
-# Okay now we need to make sure the units are correct. 
+# Precipitation conversion ----
 
 # First, we need Precipitation to be in metres
 # mm ----> m
@@ -69,13 +69,25 @@ colnames(Evapotranspiration_clean) <- c("Date", "Evapotranspiration_mm")
 
 # Convert "Rainfall" from mm to meters (creating a new column)
 Precipitation_clean$Precipitation_m <- Precipitation_clean$Precipitation_mm / 1000
+# Not working, check type
 
+str(Precipitation_clean$Precipitation_m) # Character
 
+Precipitation_clean$Precipitation_mm <- as.numeric(Precipitation_clean$Precipitation_mm) # Change to numeric
 
-# Merge the 3 datasets!# Merge the 3 datasets!`Precipitation (mm)`
+# Try again
+Precipitation_clean$Precipitation_m <- Precipitation_clean$Precipitation_mm / 1000
 
-merged_data <- Flow_clean %>%
-  left_join(Rainfall_clean, by = "Date")
+Precipitation_clean$Month <- format(Precipitation_clean$Date, "%Y-%m") # No working.
+
+# Discharge luckily came in the correct units = m3/s (cubic metres per second)
+
+# Dont worry about evapotranspiration right now!
+
+# ---- Merge 3 datasets ----
+
+merged_data <- Discharge_clean %>%
+  left_join(Precipitation_clean, by = "Date")
 
 final_merged_data <- merged_data %>%
   left_join(Evapotranspiration_clean, by = "Date")
@@ -91,65 +103,84 @@ Evapotranspiration_clean$Date <- as.character(Evapotranspiration_clean$Date)
 final_merged_data <- merged_data %>%
   left_join(Evapotranspiration_clean, by = "Date")
 
-# Filter to just 2015
+# ---- Filter to just 2015 ----
 
 # Filter for dates within the range
 filtered_data <- final_merged_data %>% filter(Date >= as.Date("2015-01-01") & Date <= as.Date("2015-12-31"))
 
-# Change to long format and have all values together so we can plot
-combined_long <- filtered_data %>%
-  pivot_longer(cols = c("Rainfall", "Flow", "Evapotranspiration"), 
-               names_to = "Variable", 
-               values_to = "Value")
-# Error, lets check they're all the same data type:
-str(filtered_data)
-# Flow and Rainfall are chr, need to be num like evpt
+# Didnt work because of NA
 
-filtered_data$Rainfall <- as.numeric(filtered_data$Rainfall)
-filtered_data$Flow <- as.numeric(filtered_data$Flow)
+# Remove columns with missing or empty names
+final_merged_data <- final_merged_data[, !is.na(colnames(final_merged_data)) & colnames(final_merged_data) != ""]
 
-# Try again
-combined_long <- filtered_data %>%
-  pivot_longer(cols = c("Rainfall", "Flow", "Evapotranspiration"), 
-               names_to = "Variable", 
-               values_to = "Value")
+# Again!
+filtered_data <- final_merged_data %>% filter(Date >= as.Date("2015-01-01") & Date <= as.Date("2015-12-31"))
 
-# ---- Data visualisation ----
+Data <- filtered_data
 
-ggplot(combined_long, aes(x = Date, y = Value, color = Variable, group = Variable)) + # When plotting multiple variables with geom_line(), must specify group aesthetic to ensure lines are drawn for EACH variable. 
+# ---- Create a new column "Month" ----
+
+# Now lets create a new column called month which makes it easier to assign parameters monthly/seasonally
+
+# Convert Date column to Date format if it isn't already
+
+Data$Date <- as.Date(Data$Date) # Make sure column date is in right format called date
+
+Data <- Data %>%
+  mutate(Month = format(Date, "%b"))  # Extracts abbreviated month names (e.g., "Jan", "Feb", etc.)
+
+# Move new month column next to date column
+Data <- Data %>%
+  select(Date, Month, everything()) 
+
+# ---- Rainfall-Runoff Model ----
+
+# The easiest way to understand this is to go through one month first, and then the rest of months will follow the same idea. 
+
+# ---- INITIAL SURFACE STORAGE (CU/M)
+
+# This is the amount of water held in the surface storage from previous months. For Jan, lets just start it on 0. 
+
+Rainfall_Runoff_Model <- Data %>%
+  mutate(Initial_surface_storage = ifelse(format(Date, "%m") == "01", 0, NA)))
+
+# ---- RAINFALL
+
+Precipitation_m <- Precipitation_clean$Precipitation_m # We already have this! 
+
+# ---- AREA 
+
+Rainfall_Runoff_Model <- Rainfall_Runoff_Model %>%
+  mutate(Area = 694) #km2
+
+# ---- TOTAL WATER INPUT
+
+# This is the total water across the whole area.
+
+Rainfall_Runoff_Model <- Rainfall_Runoff_Model %>%
+  mutate(Total_water_input = Precipitation_m * Area * 1000000) # 1million because km2 --> m2 
+
+# ---- SURFACE STORAGE 
+
+# Now, we need to calculate surface storage, thinking about evapotranspiration and interception. 
+# This is the amount after interception and after evapotranspiration. How much rainfall actually hits the ground? 
+
+# For this we can plot out evapotranspiration and see how it varies throughout the year.
+
+ggplot(Rainfall_Runoff_Model, aes(x = Month, y = Evapotranspiration_mm)) + # When plotting multiple variables with geom_line(), must specify group aesthetic to ensure lines are drawn for EACH variable. 
   geom_line() +
-  labs(title = "Rainfall, Flow, and Evapotranspiration Over Time", 
+  labs(title = "Evapotranspiration Over Time", 
        x = "Date", 
-       y = "Value") +
-  theme_minimal() +
-  scale_color_manual(values = c("red", "blue", "green"))  # Customize colors
-
-# Date x axis is just a black line becuase too many words/dates.
-# Lets change data frame to months instead.
-
-ggplot(combined_long, aes(x = Date, y = Value, color = Variable, group = Variable)) + # When plotting multiple variables with geom_line(), must specify group aesthetic to ensure lines are drawn for EACH variable. 
-  geom_line() +
-  labs(title = "Rainfall, Flow, and Evapotranspiration Over Time", 
-       x = "Date", 
-       y = "Value") +
-  theme_minimal() +
-  scale_color_manual(values = c("red", "blue", "green"))  # Customize colors
+       y = "Evapotranspiration (mm)") +
   scale_x_date(date_labels = "%b", date_breaks = "1 month")
+# Now we can see how Et varies seasonally and when it peaks. 
 
-# Not working, check Date data type is date  
-str(combined_long$Date) # Its a character
-combined_long$Date <- as.Date(combined_long$Date) # Changed to date
+# Nov-April low-none Et
+# Apr-June and Sept-Nov mid Et
+# Summer aka June-Aug VVV high!
 
-# Third time lucky
-ggplot(combined_long, aes(x = Date, y = Value, color = Variable, group = Variable)) + # When plotting multiple variables with geom_line(), must specify group aesthetic to ensure lines are drawn for EACH variable. 
-  geom_line() +
-  labs(title = "Rainfall, Flow, and Evapotranspiration Over Time", 
-       x = "Date", 
-       y = "Value") +
-  theme_minimal() +
-  scale_color_manual(values = c("red", "blue", "green")) +  # Customize colors
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") # %b displays "Jan", "Feb"
+# So what does this mean?
 
-# Got it!! 
+
 
 
